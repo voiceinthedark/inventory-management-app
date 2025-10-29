9d606eb4aeb752b37aa005e223f73b30367298b6
voiceinthedark <darkrisingforce@gmail.com>
2025-10-29 12:36:22 +0200

    refactor(genreRoutes): refactor the logic into its own controller

---
+++ controllers/genresController.js
@@ -0,0 +1,29 @@
+const db = require("../db/queries");
+
+const getGenreIndex = async (req, res) => {
+  try {
+    const genres = await db.getAllGenres();
+    res.render("genres/index", { genres });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getBooksByGenre = async (req, res) => {
+  const { genreId } = req.params;
+  try {
+    const books = await db.getBooksByGenre(genreId);
+    console.log(books);
+    res.render("genres/genre-books", {
+      genre: books.length > 0 ? books[0].genre_name : "Unknown Genre",
+      books,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  getGenreIndex,
+  getBooksByGenre,
+};
--- routes/genresRoutes.js
+++ routes/genresRoutes.js
@@ -1,31 +1,11 @@
 const express = require("express");
 const { Router } = express;
-const db = require("../db/queries");
+const genresController = require("../controllers/genresController");
 
 const genresRouter = Router();
 
 // GET /genres - Retrieve all genres
-genresRouter.get("/", async (req, res) => {
-  try {
-    const genres = await db.getAllGenres();
-    res.render("genres/index", { genres });
-  } catch (err) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-genresRouter.get("/:genreId", async (req, res) => {
-  const { genreId } = req.params;
-  try {
-    const books = await db.getBooksByGenre(genreId);
-    console.log(books);
-    res.render("genres/genre-books", {
-      genre: books.length > 0 ? books[0].genre_name : "Unknown Genre",
-      books,
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+genresRouter.get("/", genresController.getGenreIndex);
+genresRouter.get("/:genreId", genresController.getBooksByGenre);
 
 module.exports = genresRouter;
